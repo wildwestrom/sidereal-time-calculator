286635eeac5dd8511ead78cfa4154023906edd1a
Christian Westrom <c.westrom@westrom.xyz>
2023-05-07 23:39:35 +0900

    add a small command line interface

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::str::FromStr;
 
 use anyhow::{anyhow, Result};
-use chrono::{DateTime, Datelike, Duration, NaiveDate, NaiveDateTime, NaiveTime, Timelike, Utc};
+use chrono::{Datelike, Duration, NaiveDate, NaiveDateTime, NaiveTime, Timelike, Utc};
 use chrono_tz::Tz;
+use clap::Parser;
 use libastro_sys::{cal_mjd, utc_gst};
 use once_cell::sync::Lazy;
 use tzf_rs::DefaultFinder;
@@ -68,85 +69,114 @@ fn local_mean_sidereal_time(gmst: f64, longitude: f64) -> f64 {
 	24.0 * ((gmst + longitude / 15.0) / 24.0).fract()
 }
 
-fn info_text(utc_datetime: DateTime<Utc>, latitude: f64, longitude: f64) -> Result<String> {
+fn display_info(latitude: Option<f64>, longitude: f64) -> Result<()> {
 	const TIME_FMT_STRING: &str = "%T.%6f";
 	const TIME_ZONE_FMT_STRING: &str = "%T.%6f %z/%Z";
 
-	let mut info = String::new();
-	// don't use more than two digits of precision for coordinates
-	let timezone: Tz = get_timezone(latitude, longitude).expect("No timezone");
+	let term = console::Term::buffered_stdout();
 
-	info.push_str(&format!(
-		"           Zone for {:>5.1}, {:>5.1}: {:?}\n",
-		latitude, longitude, timezone
-	));
+	let timezone;
 
-	let curr_date = utc_datetime.date_naive();
+	if let Some(latitude) = latitude {
+		timezone = get_timezone(latitude, longitude).ok();
+	} else {
+		timezone = None
+	}
 
-	info.push_str(&format!(
-		"                  Gregorian Date: {}\n",
-		curr_date
-	));
+	loop {
+		let utc_datetime = Utc::now();
 
-	info.push_str(&format!(
-		"                  Universal Time: {}\n",
-		utc_datetime.format(TIME_ZONE_FMT_STRING)
-	));
+		let mut info = String::new();
 
-	let local_time = utc_datetime.with_timezone(&timezone);
-	info.push_str(&format!(
-		"                      Local Time: {}\n",
-		local_time.format(TIME_ZONE_FMT_STRING)
-	));
+		if let (Some(latitude), Some(timezone)) = (latitude, timezone) {
+			info.push_str(&format!(
+				"           Zone for {:>5.1}, {:>5.1}: {:?}\n",
+				latitude, longitude, timezone
+			));
 
-	let mjd = mjd_from_gregorian_datetime(utc_datetime.naive_utc());
-	info.push_str(&format!("             Modified Julian Day: {}\n", mjd));
-
-	let greenwich_mst = greenwich_mean_sidereal_time(utc_datetime.naive_utc());
-	info.push_str(&format!(
-		"    Greenwich mean Sidereal Time: {} \n",
-		decimal_to_time(greenwich_mst)?.format(TIME_FMT_STRING)
-	));
-
-	let local_mst = local_mean_sidereal_time(greenwich_mst, longitude);
-	info.push_str(&format!(
-		"        Local mean Sidereal Time: {}\n",
-		decimal_to_time(local_mst)?.format(TIME_FMT_STRING)
-	));
-
-	let time_until_peak = {
-		static SPOTISWOODE_PEAK_TIME: Lazy<NaiveTime> =
-			Lazy::new(|| NaiveTime::from_hms_opt(13, 30, 0).unwrap());
-
-		let duration = SPOTISWOODE_PEAK_TIME.signed_duration_since(decimal_to_time(local_mst)?);
-		if duration.lt(&Duration::zero()) {
-			// If the duration is negative, add 24 hours to it to get the time until the next occurrence.
-			duration + chrono::Duration::hours(24)
+			let local_time = utc_datetime.with_timezone(&timezone);
+			info.push_str(&format!(
+				"                      Local Time: {}\n",
+				local_time.format(TIME_ZONE_FMT_STRING)
+			));
 		} else {
-			duration
+			info.push_str(&format!(
+				"                       Longitude: {}\n",
+				longitude
+			))
 		}
-	};
-	info.push_str(&format!(
-		"Time Until Spotiswoode Peak Time: {}\n",
-		decimal_to_time(
-			time_until_peak.num_nanoseconds().unwrap() as f64 / 1_000_000_000.0 / 60.0 / 60.0
-		)?
-		.format(TIME_FMT_STRING),
-	));
 
-	Ok(info)
+		let curr_date = utc_datetime.date_naive();
+
+		info.push_str(&format!(
+			"                  Gregorian Date: {}\n",
+			curr_date
+		));
+
+		info.push_str(&format!(
+			"                  Universal Time: {}\n",
+			utc_datetime.format(TIME_ZONE_FMT_STRING)
+		));
+
+		let mjd = mjd_from_gregorian_datetime(utc_datetime.naive_utc());
+		info.push_str(&format!("             Modified Julian Day: {}\n", mjd));
+
+		let greenwich_mst = greenwich_mean_sidereal_time(utc_datetime.naive_utc());
+		info.push_str(&format!(
+			"    Greenwich mean Sidereal Time: {} \n",
+			decimal_to_time(greenwich_mst)?.format(TIME_FMT_STRING)
+		));
+
+		let local_mst = local_mean_sidereal_time(greenwich_mst, longitude);
+		info.push_str(&format!(
+			"        Local mean Sidereal Time: {}\n",
+			decimal_to_time(local_mst)?.format(TIME_FMT_STRING)
+		));
+
+		let time_until_peak = {
+			static SPOTISWOODE_PEAK_TIME: Lazy<NaiveTime> =
+				Lazy::new(|| NaiveTime::from_hms_opt(13, 30, 0).unwrap());
+
+			let duration = SPOTISWOODE_PEAK_TIME.signed_duration_since(decimal_to_time(local_mst)?);
+			if duration.lt(&Duration::zero()) {
+				// If the duration is negative, add 24 hours to it to get the time until the next occurrence.
+				duration + chrono::Duration::hours(24)
+			} else {
+				duration
+			}
+		};
+		info.push_str(&format!(
+			"Time Until Spotiswoode Peak Time: {}",
+			decimal_to_time(
+				time_until_peak.num_nanoseconds().unwrap() as f64 / 1_000_000_000.0 / 60.0 / 60.0
+			)?
+			.format(TIME_FMT_STRING),
+		));
+
+		let lines_to_clear = info.chars().into_iter().filter(|c| *c == '\n').count();
+
+		term.write_line(&info)?;
+		term.flush()?;
+		term.clear_last_lines(lines_to_clear + 1)?;
+		std::thread::sleep(std::time::Duration::from_micros(200));
+	}
+}
+
+#[derive(Parser, Debug)]
+#[command(name = "sidtime")]
+/// Prints shows the local sidereal time given a longitude.
+struct Cli {
+	/// Latitude
+	#[arg(long)]
+	lat: Option<f64>,
+	/// Longitude (+ for E - for W)
+	#[arg(long)]
+	lon: f64,
 }
 
 fn main() -> Result<()> {
-	const LATITUDE: f64 = 36.755;
-	const LONGITUDE: f64 = 127.869;
-	let term = console::Term::buffered_stdout();
+	let cli = Cli::parse();
 
-	loop {
-		let info = info_text(Utc::now(), LATITUDE, LONGITUDE)?;
-		term.write_line(&info)?;
-		term.flush()?;
-		term.clear_last_lines(9)?;
-		std::thread::sleep(std::time::Duration::from_micros(200));
-	}
+	let _ = display_info(cli.lat, cli.lon);
+	Ok(())
 }
